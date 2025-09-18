78917f5a91d8739997fa8de4fd07884995d05bfc
aquib-45 <aquibrehan45@gmail.com>
2025-09-18 19:48:02 +0530

    Update Prime.c

--- Week1/Prime.cpp
+++ Week1/Prime.cpp
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int n, i;
+    int arr[100];
+    int largest, smallest;
+
+    printf("Enter number of elements in array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Assume first element is both largest and smallest
+    largest = smallest = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+        if(arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    printf("Largest element = %d\n", largest);
+    printf("Smallest element = %d\n", smallest);
+
+    return 0;
+}
